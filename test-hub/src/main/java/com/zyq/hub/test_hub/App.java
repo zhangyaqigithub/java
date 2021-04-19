3f627a223cc7491763d2dc7f3198266ea40acbcc
zyq <992321066@qq.com>
2021-04-19 19:30:59 +0800

    for local commit

---
+++ test-hub/src/main/java/com/zyq/hub/test_hub/App.java
@@ -0,0 +1,13 @@
+package com.zyq.hub.test_hub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
